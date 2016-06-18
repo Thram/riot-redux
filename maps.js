a01c4323fe949681dac516d0e26f3136f3710157
Thram <thramposo@gmail.com>
2016-06-18 21:17:01 +1200

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,26 @@
+/**
+ * Created by thram on 16/06/16.
+ */
+import {reduce} from "lodash";
+import {Payload, isEvent} from "toolbox/utils";
+
+export function payload(state) {
+  return state.payload || {};
+}
+
+export function actions(methods, dispatch) {
+  return _.reduce(methods, function (result, method, key) {
+    result[key] = function (options) {
+      if (isEvent(options)) {
+        options.stopPropagation();
+        options.preventDefault();
+      }
+      return dispatch(Payload(key, method(options)));
+    };
+    return result;
+  }, {});
+}
+
+export function defaultReducer(state, action) {
+  return action;
+}
