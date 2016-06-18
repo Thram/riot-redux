484064af858d5adeea55e8b0de58b8d49c37fc60
Thram <thramposo@gmail.com>
2016-06-18 21:17:39 +1200

    Update maps.js

--- maps.js
+++ maps.js
@@ -2,7 +2,7 @@
  * Created by thram on 16/06/16.
  */
 import {reduce} from "lodash";
-import {Payload, isEvent} from "toolbox/utils";
+import {Payload, isEvent} from "utils";
 
 export function payload(state) {
   return state.payload || {};
